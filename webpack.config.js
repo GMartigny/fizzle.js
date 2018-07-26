bdae86ce4199b036d6d2c48b86949918a03e34e3
Guillaume Martigny <guillaume.martigny@wiztivi.com>
2018-07-26 12:11:05 +0200

    Update packages Use Webpack v4 and remove all deprecated dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,7 @@
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-
 module.exports = {
     entry: "./fizzle.js",
-    plugins: [
-        new UglifyJsPlugin(),
-    ],
-    module: {
-        rules: [{
-            test: /\.js$/,
-            use: {
-                loader: "babel-loader",
-                options: {
-                    presets: ["@babel/preset-env"],
-                },
-            },
-        }],
-    },
     output: {
-        filename: "dist/fizzle.min.js",
+        filename: "fizzle.min.js",
         library: "Fizzle",
         libraryTarget: "this",
         libraryExport: "default",
